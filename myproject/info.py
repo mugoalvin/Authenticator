9c1195d75ef8e833a1ebda7c13626fdfc19ed459
mugoalvin <119125651+mugoalvin@users.noreply.github.com>
2023-03-31 22:00:27 +0300

    Git Commit

---
+++ myproject/info.py
@@ -0,0 +1,7 @@
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'frashasanto111@gmail.com' #your email for example tunge254@gmail.com
+EMAIL_HOST_PASSWORD = 'nqzkgoqtwsgxmcjt' #your email password
+EMAIL_PORT = 587
+
